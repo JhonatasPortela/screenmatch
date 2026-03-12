e4fbe18215ab2cd488aa56fc5de6818c497598c5
Jhonatas Portela <jhonatasportela1997@gmail.com>
2026-03-12 07:32:49 -0300

    Criando o conversor de dados

--- src/main/java/br/app/portela/screenmatch/ScreenmatchApplication.java
+++ src/main/java/br/app/portela/screenmatch/ScreenmatchApplication.java
@@ -1,6 +1,8 @@
 package br.app.portela.screenmatch;
 
+import br.app.portela.screenmatch.model.DadosSerie;
 import br.app.portela.screenmatch.service.ConsumoApi;
+import br.app.portela.screenmatch.service.ConverteDados;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -15,8 +17,12 @@ public class ScreenmatchApplication implements CommandLineRunner {
 	@Override
 	public void run(String... args) throws Exception {
 		ConsumoApi consumoApi = new ConsumoApi();
-		String json = consumoApi.obterDados("https://www.omdbapi.com/?apikey=92a14529&t=matrix");
+		String json = consumoApi.obterDados("https://www.omdbapi.com/?apikey=92a14529&t=the+walking+dead");
 		System.out.println(json);
+		ConverteDados conversor = new ConverteDados();
+		DadosSerie serie = conversor.obterDados(json, DadosSerie.class);
+		System.out.println(serie);
+
 
 	}
 
--- src/main/java/br/app/portela/screenmatch/model/DadosSerie.java
+++ src/main/java/br/app/portela/screenmatch/model/DadosSerie.java
@@ -1,8 +1,10 @@
 package br.app.portela.screenmatch.model;
 
 import com.fasterxml.jackson.annotation.JsonAlias;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
+@JsonIgnoreProperties(ignoreUnknown = true)
 public record DadosSerie(@JsonAlias("Title") String titulo,
-                         @JsonAlias("totalSeasons") Integer temporadas,
+                         @JsonAlias("totalSeasons") Integer totalTemporadas,
                          @JsonAlias("imdbRating") String avaliacao) {
 }
