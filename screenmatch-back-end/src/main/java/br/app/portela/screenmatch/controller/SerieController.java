84a67fdfcd948111c64803105c511622bdd69897
JhonatasPortela <jhonatasportela1997@gmail.com>
2026-07-15 06:59:53 -0300

    Adicionando Spring Boot DevTools para suporte a reinício automático e LiveReload, além de implementar um novo endpoint "/inicio" no SerieController que retorna uma mensagem de teste.

--- screenmatch-back-end/src/main/java/br/app/portela/screenmatch/controller/SerieController.java
+++ screenmatch-back-end/src/main/java/br/app/portela/screenmatch/controller/SerieController.java
@@ -1,15 +1,14 @@
 package br.app.portela.screenmatch.controller;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.app.portela.screenmatch.dto.SerieDto;
-import br.app.portela.screenmatch.model.Serie;
 import br.app.portela.screenmatch.repository.SerieRepository;
-import java.util.stream.Collectors;
 
 @RestController
 public class SerieController {
@@ -23,5 +22,10 @@ public class SerieController {
         .stream()
         .map(s -> new SerieDto(s.getId(), s.getTitulo(), s.getTotalTemporadas(), s.getAvaliacao(), s.getGenero(), s.getAtores(), s.getPoster(), s.getSinopse()))
         .collect(Collectors.toList());
+    }
+
+    @GetMapping("/inicio")
+    public String paginaInicial() {
+        return "TESTE DE PÁGINA INICIAL";
     }
 }
